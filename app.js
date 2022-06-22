37ea77829e4ce975f2301658b7e4d53979af093e
akinlope <akinrimisitt@gmail.com>
2022-06-22 14:10:46 +0100

    completed

---
+++ app.js
@@ -0,0 +1,12 @@
+console.log("connected..");
+let nam = document.querySelector(".name");
+let height = document.querySelector(".height");
+let country = document.querySelector(".country");
+
+const myName = "Akinrimisi Tolulope S";
+const myHeight = "5.7ft"
+const myCountry = "Nigeria"
+
+nam.innerHTML = `My name is: ${myName}`
+height.innerHTML = `My height is ${myHeight} `
+country.innerHTML = `I'm from ${myCountry}`
